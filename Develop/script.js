76cd0bad8a340e7202a09c63c86865a1707606c2
Kevin Lyons <zinckev@hotmail.com>
2020-12-17 10:00:48 -0800

    ditched luxon and swapped to moment

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,17 @@
-var DateTime = luxon.DateTime;
+// const moment = require("./moment");
+var currentDate = moment().format(`MMMM/DD/YYYY`);
+var dateTime = $("#currentDay");
 
-$("#currentDay").text(DateTime.local(DATE_FULL()));
+$("#currentDay").text(currentDate);
+var textArea = $(".text-input");
+var texPull = JSON.parse(localStorage.getItem(textArea)) || [];
+// var savedText = [(nine: {})];
+// this area is where luxon would go if it actually worked like it says it does
+
+// this area is where luxon would go for choosing colors of text area on time if it functioned like it should
+
+$("button").click(function () {
+  console.log("clicked");
+
+  localStorage.setItem("text-input", JSON.stringify(texPull));
+});
